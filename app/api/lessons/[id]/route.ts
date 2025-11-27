d373593dfe8dff8fadcefe9e3646597cebd6b16f
SaidSafarov09 <93523342+SaidSafarov09@users.noreply.github.com>
2025-11-27 23:34:54 +0300

    коммит

--- app/api/lessons/[id]/route.ts
+++ app/api/lessons/[id]/route.ts
@@ -16,6 +16,7 @@ export async function GET(
     request: Request,
     { params }: { params: Promise<{ id: string }> }
 ) {
+    
     try {
         const { id } = await params
         const session = await getServerSession(authOptions)
