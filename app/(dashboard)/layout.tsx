ddc3ce24d61f031079319f2f781cacc845cfa276
SaidSafarov09 <93523342+SaidSafarov09@users.noreply.github.com>
2025-12-04 13:40:31 +0300

    добавление провайдеров

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import { DashboardLayout } from '@/components/layout/DashboardLayout'
+import { AuthProvider } from '@/components/providers/AuthProvider'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <DashboardLayout>{children}</DashboardLayout>
+        <AuthProvider>
+            <DashboardLayout>{children}</DashboardLayout>
+        </AuthProvider>
     )
 }
