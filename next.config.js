77113e5220bdd616d0f1770f0a8d05c0ad565c42
SaidSafarov09 <93523342+SaidSafarov09@users.noreply.github.com>
2025-12-16 02:21:57 +0300

    Переменные 4

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 
-const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("@prisma/client");
+    }
+    return config;
+  },
+};
 
 module.exports = nextConfig
