bf2e61b154dae02917c0ba1e6a84b39501b25df5
SaidSafarov09 <93523342+SaidSafarov09@users.noreply.github.com>
2025-12-16 05:09:32 +0300

    конфиги

--- next.config.js
+++ next.config.js
@@ -5,6 +5,25 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-}
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: `
+                            default-src 'self';
+                            script-src 'self' yastatic.net yandex.st mc.yandex.ru 'unsafe-eval';
+                            connect-src 'self' passport.yandex.ru mc.yandex.ru yandex.ru autofill.yandex.ru;
+                            img-src 'self' data: mc.yandex.ru yastatic.net;
+                            style-src 'self' 'unsafe-inline';
+                        `.replace(/\n/g, ''),
+                    },
+                ],
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
