06698c9a7fc1de105f28f6ee6506af553933195d
SaidSafarov09 <93523342+SaidSafarov09@users.noreply.github.com>
2025-12-16 20:11:43 +0300

    изменение скоупов

--- app/api/auth/yandex/login/route.ts
+++ app/api/auth/yandex/login/route.ts
@@ -4,9 +4,15 @@ export async function GET() {
     const clientId = process.env.YANDEX_CLIENT_ID
     console.log('Yandex Client ID:', clientId)
     const redirectUri = `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/yandex/callback`
-    const scope = 'login:email login:info login:avatar login:birthday login:default_phone'
 
-    const yandexAuthUrl = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`
+    const params = new URLSearchParams({
+        response_type: 'code',
+        client_id: clientId || '',
+        redirect_uri: redirectUri,
+        scope: 'login:email login:info login:avatar login:birthday login:default_phone'
+    })
+
+    const yandexAuthUrl = `https://oauth.yandex.ru/authorize?${params.toString()}`
 
     return NextResponse.redirect(yandexAuthUrl)
 }
