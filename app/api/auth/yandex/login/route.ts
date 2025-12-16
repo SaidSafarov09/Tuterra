d7d8b1a995f1e3ee929b5a9a177cb98accdc2b61
SaidSafarov09 <93523342+SaidSafarov09@users.noreply.github.com>
2025-12-16 03:38:46 +0300

    попытка 18

--- app/api/auth/yandex/login/route.ts
+++ app/api/auth/yandex/login/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
     const clientId = process.env.YANDEX_CLIENT_ID
+    console.log('Yandex Client ID:', clientId)
     const redirectUri = `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/yandex/callback`
     const scope = 'login:email login:info login:avatar login:birthday login:default_phone'
 
