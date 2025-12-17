247b02e362e92bf0b3b46807f6ad5120e3b64a45
SaidSafarov09 <93523342+SaidSafarov09@users.noreply.github.com>
2025-12-18 01:36:06 +0300

    очередные конфиги

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-}
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' 
+                https://yastatic.net 
+                https://mc.yandex.com 
+                https://accounts.google.com;
+              connect-src 'self' 
+                https://passport.yandex.ru 
+                https://mc.yandex.com 
+                https://yandex.ru 
+                https://accounts.google.com;
+              img-src 'self' 
+                data: 
+                https://avatars.yandex.net 
+                https://yandex.st 
+                https://mc.yandex.com 
+                https://img.youtube.com 
+                https://lh3.googleusercontent.com;
+              style-src 'self' 'unsafe-inline';
+              font-src 'self';
+            `.replace(/\s{2,}/g, " "),
+                    },
+                ],
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
