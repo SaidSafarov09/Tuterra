6fd29bced260dcf5488974ec773b33e04d2f0968
SaidSafarov09 <93523342+SaidSafarov09@users.noreply.github.com>
2026-01-01 21:15:37 +0300

    изменение бейджика про

--- components/layout/ProBadge.tsx
+++ components/layout/ProBadge.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './ProBadge.module.scss'
 import { CrownIcon } from '@/components/icons/Icons'
+import { useAuthStore } from '@/store/auth'
 
-interface ProBadgeProps {
-    isPro?: boolean
-}
+export const ProBadge: React.FC = () => {
+    const { user } = useAuthStore()
 
-export const ProBadge: React.FC<ProBadgeProps> = ({ isPro }) => {
+    if (user?.plan !== 'pro') return null
+
     return (
         <div className={styles.proContainer}>
             <CrownIcon size={20} className={styles.proIcon} />
